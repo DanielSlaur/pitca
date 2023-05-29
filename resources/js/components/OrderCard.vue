51a412a67d1bd1f2fa16b2879b4f7e33662fd2a2
DanielSlaur <danielslaur@gmail.com>
2023-05-29 15:54:13 +0300

    feat(app): added some components

---
+++ resources/js/components/OrderCard.vue
@@ -0,0 +1,17 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "OrderCard"
+})
+</script>
+
+<template>
+    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow  ">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
