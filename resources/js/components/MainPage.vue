af7a6887ffc482a05f6f8f96e4f28a6c6ddcd375
DanielSlaur <danielslaur@gmail.com>
2023-05-29 15:55:10 +0300

    feat(app): added pages and navbar

--- resources/js/components/MainPage.vue
+++ resources/js/components/MainPage.vue
@@ -5,12 +5,13 @@ import {ref} from "vue";
 import Cart from "@/components/Cart.vue";
 import { useCartStore } from '../store'
 import {mapStores} from "pinia";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
-    components: {Cart, PitcaItem, PButton},
+    components: {Navbar, Cart, PitcaItem, PButton},
     data(){
         return{
-            pizzas: [1, 2 , 3, 4],
+            pizzas: [1, 2 , 3, 4, 5, 6, 7, 8],
             // pizzasCount: this.
         }
     },
@@ -32,17 +33,9 @@ export default {
 
 <template>
     <header>
-        <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 border-b">
-            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                <a href="https://flowbite.com" class="flex items-center">
-                    <img src="../../images/logo.png" class="mr-3 h-6 sm:h-9" alt="Pitca Logo" />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Pitca</span>
-                </a>
-                <div class="flex items-center lg:order-2">
-                   <p-button class="flex items-center" @click="openCart()">Корзина <span v-if="cartStore.count > 0" class="withItems"></span> <span v-if="cartStore.count > 0" class="ml-2 mr-2">{{cartStore.count}} </span></p-button>
-                </div>
-            </div>
-        </nav>
+        <Navbar>
+            <p-button class="flex items-center" @click="openCart()">Корзина <span v-if="cartStore.count > 0" class="withItems"></span> <span v-if="cartStore.count > 0" class="ml-2 mr-2">{{cartStore.count}} </span></p-button>
+        </Navbar>
     </header>
     <div class=" flex  flex-wrap justify-between items-center mx-auto max-w-screen-xl mt-5">
         <pitca-item v-for="pizza in pizzas"></pitca-item>
---
+++ resources/js/components/OrderPage.vue
@@ -0,0 +1,53 @@
+<script>
+
+import Navbar from "@/components/Navbar.vue";
+import OrderCard from "@/components/OrderCard.vue";
+import PizzaItemOrder from "@/components/PizzaItemOrder.vue";
+import {defineComponent} from "vue";
+import {mapStores} from "pinia";
+import {useCartStore} from "@/store/index.js";
+
+export default defineComponent({
+    name: "OrderPage",
+    components:{Navbar, OrderCard, PizzaItemOrder},
+    props:{
+    },
+    computed: {
+        ...mapStores(useCartStore)
+    },
+
+    methods:{
+    }
+})
+</script>
+
+<template>
+    <header>
+        <Navbar></Navbar>
+    </header>
+    <div class="flex flex-column justify-around mt-10">
+        <div>
+            <OrderCard>
+                <div class="w-50">
+                    <h1 class="font-bold text-xl">Заказ</h1>
+                    <PInput>Имя</PInput>
+                    <PInput>Номер телефона</PInput>
+                    <PInput>Адрес</PInput>
+                </div>
+            </OrderCard>
+            <OrderCard class="mt-5 bg-gray-200">
+                <span class="font-semibold">Способ оплаты</span>
+            </OrderCard>
+        </div>
+        <OrderCard>
+            <span class="font-light">Состав заказа</span>
+            <div>
+                <PizzaItemOrder v-for="pizza in cartStore.pizzas" :pizza="pizza"></PizzaItemOrder>
+            </div>
+        </OrderCard>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
