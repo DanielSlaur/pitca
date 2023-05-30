63a81a3ae62b0a436f43d8ce10629a2036e2ea60
DanielSlaur <danielslaur@gmail.com>
2023-05-30 11:03:34 +0300

    feat(app): make seeders and factory

--- app/Http/Controllers/Api/PizzasController.php
+++ app/Http/Controllers/Api/PizzasController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pizzas;
 use Illuminate\Http\Request;
 
 class PizzasController extends Controller
 {
-    //
+    public function index(){
+        return Pizzas::all();
+    }
 }
