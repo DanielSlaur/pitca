92838cf70378a19ed9e88015e4a58b65c3babd8d
DanielSlaur <danielslaur@gmail.com>
2023-06-01 14:37:24 +0300

    fix(app): some style fixes

--- resources/js/components/PButton.vue
+++ resources/js/components/PButton.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full">
+    <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full disabled:bg-gray-400 transition">
         <slot></slot>
     </button>
 </template>
